edc0c218439291844b2120233e1a187f07650ec0
ritheshh-cmykkkk <stake15432@gmail.com>
2025-06-27 18:50:16 +0530

    Add PM2 ecosystem.config.js for process management

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [
+    {
+      name: "backendserver",
+      script: "./server.js",
+      watch: true,
+      env: {
+        NODE_ENV: "production"
+      }
+    },
+    {
+      name: "duckdns-updater",
+      script: "./duckdns-updater.sh",
+      interpreter: "/data/data/com.termux/files/usr/bin/bash"
+    }
+  ]
+} 
